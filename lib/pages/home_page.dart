f88374e43fb903f3a7878076a41d884afac21e76
Felipe Mantoan Pardim <felipemantoan@hotmail.com>
2023-09-09 11:55:50 -0300

    feat (columns): columns rows

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var generateNumber = 0;
+  var quantityClick = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +22,75 @@ class _HomePageState extends State<HomePage> {
           // style: GoogleFonts.pacifico()
         ),
       ),
-      body: Center(
-          child: Text(
-        generateNumber.toString(),
-        // style: GoogleFonts.acme(fontSize: 20),
-      )),
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.cyan,
+              child: const Text(
+                "Ações do usuário",
+                // style: GoogleFonts.acme(fontSize: 20),
+              ),
+            ),
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Container(
+                color: Colors.orange,
+                child: Text(
+                  "Foi clicado $quantityClick vezes",
+                  // style: GoogleFonts.acme(fontSize: 20),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.indigo,
+                child: Text(
+                  "O número gerado foi $generateNumber",
+                  // style: GoogleFonts.acme(fontSize: 20),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                            color: Colors.red, child: const Text("Nome: "))),
+                    Expanded(
+                        flex: 3,
+                        child: Container(
+                            color: Colors.blue,
+                            child: const Text("Felipe Pardim  "))),
+                    Expanded(
+                        flex: 2,
+                        child: Container(
+                            color: Colors.green, child: const Text("30"))),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () => {
                 setState(() {
+                  quantityClick++;
+
                   generateNumber =
                       GenerateRandomNumberService.generateRandomNumber(100000);
                 })
